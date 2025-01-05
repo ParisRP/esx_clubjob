b9fe2ce80850e82d2c6a1e86a16c5a737993ad31
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-05 21:18:33 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'parisrp'
+description 'Job Club réaliste pour ESX Legacy'
+version '1.1.0'
+
+shared_scripts { '@es_extended/imports.lua' }
+server_scripts { 'server.lua' }
+client_scripts { 'client.lua' }
+dependencies { 'es_extended' }
